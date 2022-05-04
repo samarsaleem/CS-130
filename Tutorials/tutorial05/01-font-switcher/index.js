eae2bd389ae0c69941e53a41aeb49c55661c7eff
Samar Saleem <samarsaleem2025@u.northwestern.edu>
2022-05-04 16:27:07 -0500

    commit

---
+++ Tutorials/tutorial05/01-font-switcher/index.js
@@ -0,0 +1,31 @@
+const makeBigger = () => {
+   alert('make bigger!');
+};
+
+const makeSmaller = () => {
+   alert('make smaller!');
+};
+
+/*
+document.querySelector(???).addEventListener('click', makeBigger);
+document.querySelector(???).addEventListener('click', makeSmaller);
+*/
+
+let currentFontSize = 1.4;
+
+const makeBigger = () => {
+   currentFontSize = currentFontSize +0.2;
+   document.querySelector('.content').style.fontSize = '${currentFontSize}em';
+   document.querySelector('h1').style.fontSize = '${currentFontSize + 0.5}em';
+};
+const makeSmaller = () => {
+   currentFontSize = currentFontSize - 0.2;
+   document.querySelector('.content').style.fontSize = '${currentFontSize}em';
+   document.querySelector('h1').style.fontSize = '${currentFontSize + 0.5}em';
+};
+const setFontSize = () => {
+   document.querySelector('.content').style.fontSize= '${currentFontSize}em';
+   document.querySelector('h1').style.fontSize = '${currentFontSize + 0.5}em';
+};
+document.querySelector('#a1').addEventListener('click', makeBigger);
+document.querySelector('#a2').addEventListener('click', makeSmaller);

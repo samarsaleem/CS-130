56d1f7feca5f07d01fdc5cc2c70bbbaa0fe0e1ff
Samar Saleem <samarsaleem2025@u.northwestern.edu>
2022-05-26 01:38:55 -0500

    1

--- Tutorials/tutorial05/01-font-switcher/index.js
+++ Tutorials/tutorial05/01-font-switcher/index.js
@@ -6,6 +6,8 @@
 //    alert('make smaller!');
 // };
 
+document.querySelector('#a1').addEventListener('click', makeBigger);
+document.querySelector('#a2').addEventListener('click', makeSmaller);
 
 let currentFontSize = 1.4;
 
@@ -23,5 +25,3 @@ const setFontSize = () => {
    document.querySelector('div.content').style.fontSize= '${currentFontSize}em';
    document.querySelector('h1').style.fontSize = '${currentFontSize + 0.5}em';
 };
-document.querySelector('#a1').addEventListener('click', makeBigger);
-document.querySelector('#a2').addEventListener('click', makeSmaller);
